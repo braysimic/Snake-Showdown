70e36bb7436b06d3569988c14a0d3bfba332cfd0
Brayden <bsimic@uco.edu>
2025-04-09 16:16:50 -0500

    implemented game state

--- view/statePattern/GameStateOver.java
+++ view/statePattern/GameStateOver.java
@@ -1,12 +1,18 @@
 package view.statePattern;
 
+import controller.App;
 import view.AppWindow;
 
 public class GameStateOver implements GameState{
 
+	public GameStateOver() {
+		App.win.restartButton.setEnabled(false);
+		App.win.startPauseButton.setEnabled(true);
+	}
+
 	@Override
 	public void goNext(AppWindow context) {
-		
+		context.setGameState(new GameStateInit());
 	}
 
 	@Override
--- view/statePattern/GameStatePaused.java
+++ view/statePattern/GameStatePaused.java
@@ -1,12 +1,19 @@
 package view.statePattern;
 
+import controller.App;
 import view.AppWindow;
 
 public class GameStatePaused implements GameState{
 
+	public GameStatePaused() {
+		App.win.startPauseButton.setText(AppWindow.START_ACTION);
+		App.win.restartButton.setEnabled(false);
+		App.win.startPauseButton.setEnabled(true);
+	}
+
 	@Override
 	public void goNext(AppWindow context) {
-		
+		context.setGameState(new GameStatePlaying());
 	}
 
 	@Override
